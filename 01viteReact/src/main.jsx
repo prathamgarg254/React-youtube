0006b2df8afcd8e670f40a553b99b8ff61a7f98e
Pratham Garg <gargpratham254@gmail.com>
2024-07-02 13:40:45 +0530

    creating react element

--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click me to visit google',
+    anotherUser
+
+    //Don't evalauate in this , these are output ready 
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+    reactElement
   
 )
